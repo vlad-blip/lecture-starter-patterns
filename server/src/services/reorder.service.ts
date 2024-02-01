99c64442a8423e879b9ca7230abf84a9b10f41c3
Vladyslav <ezio2506@gmail.com>
2024-02-01 22:20:42 +0200

    + added proxy and obsorver for service

--- server/src/services/reorder.service.ts
+++ server/src/services/reorder.service.ts
@@ -1,5 +1,9 @@
-import { Card } from '../data/models/card';
-import { List } from '../data/models/list';
+import { Card } from "../data/models/card";
+import { List } from "../data/models/list";
+
+import { Observer, type IObserver } from "./observer.service";
+
+const observer = new Observer();
 
 export class ReorderService {
   public reorder<T>(items: T[], startIndex: number, endIndex: number): T[] {
@@ -51,5 +55,80 @@ export class ReorderService {
 
   private insert<T>(items: T[], index: number, value: T): T[] {
     return [...items.slice(0, index), value, ...items.slice(index)];
+  }
+}
+
+// PATTERN: Proxy pattern
+
+export class ReorderProxy {
+  private reorderService: ReorderService;
+
+  private observers: IObserver[] = [];
+
+  constructor(reorderService: ReorderService) {
+    this.reorderService = reorderService;
+  }
+
+  // PATTERN: Observer
+  public attach(observer: IObserver): void {
+    const isExisting = this.observers.includes(observer);
+
+    if (!isExisting) {
+      this.observers.push(observer);
+    }
+  }
+
+  public detach(observer: IObserver): void {
+    const observerIndex = this.observers.indexOf(observer);
+
+    if (observerIndex !== -1) {
+      this.observers.splice(observerIndex, 1);
+    }
+  }
+
+  private log(data: any): void {
+    this.observers.forEach((observer) => observer.update(data));
+  }
+
+  public reorder<T>(items: T[], startIndex: number, endIndex: number): T[] {
+    this.attach(observer);
+
+    this.log(
+      `REORDER items: ${JSON.stringify(
+        items
+      )}, startIndex: ${startIndex}, endIndex: ${endIndex}`
+    );
+
+    return this.reorderService.reorder(items, startIndex, endIndex);
+  }
+
+  public reorderCards({
+    lists,
+    sourceIndex,
+    destinationIndex,
+    sourceListId,
+    destinationListId,
+  }: {
+    lists: List[];
+    sourceIndex: number;
+    destinationIndex: number;
+    sourceListId: string;
+    destinationListId: string;
+  }): List[] {
+    this.attach(observer);
+
+    this.log(
+      `REORDER lists: ${JSON.stringify(
+        lists
+      )}, sourceIndex: ${sourceIndex}, destinationIndex: ${destinationIndex}, sourceListId: ${sourceListId}, destinationListId: ${destinationListId}`
+    );
+
+    return this.reorderService.reorderCards({
+      lists,
+      sourceIndex,
+      destinationIndex,
+      sourceListId,
+      destinationListId,
+    });
   }
 }
