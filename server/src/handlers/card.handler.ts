aa8654028f0c7ab11e78f8e7284889a22c2b0309
Vladyslav <ezio2506@gmail.com>
2024-02-01 22:23:43 +0200

    + implemented delete, rename, description change and copy handlers

--- server/src/handlers/card.handler.ts
+++ server/src/handlers/card.handler.ts
@@ -1,24 +1,93 @@
-import type { Socket } from 'socket.io';
+import type { Socket } from "socket.io";
 
-import { CardEvent } from '../common/enums';
-import { Card } from '../data/models/card';
-import { SocketHandler } from './socket.handler';
+import { CardEvent } from "../../../common/enums";
+import { Card } from "../data/models/card";
+import { SocketHandler } from "./socket.handler";
+import { Observer } from "../services/observer.service";
+
+const observer = new Observer();
 
 export class CardHandler extends SocketHandler {
   public handleConnection(socket: Socket): void {
     socket.on(CardEvent.CREATE, this.createCard.bind(this));
     socket.on(CardEvent.REORDER, this.reorderCards.bind(this));
+    socket.on(CardEvent.DELETE, this.deleteCard.bind(this));
+    socket.on(CardEvent.RENAME, this.renameCard.bind(this));
+    socket.on(CardEvent.CHANGE_DESCRIPTION, this.changeDescription.bind(this));
+    socket.on(CardEvent.COPY, this.copyCard.bind(this));
   }
 
   public createCard(listId: string, cardName: string): void {
-    const newCard = new Card(cardName, '');
+    this.attach(observer);
+
+    const newCard = new Card(cardName, "");
     const lists = this.db.getData();
 
     const updatedLists = lists.map((list) =>
-      list.id === listId ? list.setCards(list.cards.concat(newCard)) : list,
+      list.id === listId ? list.setCards(list.cards.concat(newCard)) : list
     );
 
     this.db.setData(updatedLists);
+    this.updateLists();
+
+    this.log(JSON.stringify(newCard));
+  }
+
+  public deleteCard(listId: string, cardId: string): void {
+    const lists = this.db.getData();
+
+    const updatedLists = lists.map((list) =>
+      list.id === listId
+        ? list.setCards(list.cards.filter((card) => card.id !== cardId))
+        : list
+    );
+
+    this.db.setData(updatedLists);
+    this.updateLists();
+  }
+
+  public renameCard(listId: string, cardId: string, name: string): void {
+    const lists = this.db.getData();
+
+    const list = lists.find((list) => list.id === listId);
+
+    const card = list.cards.find((card) => card.id === cardId);
+
+    card.name = name;
+
+    this.db.setData(lists);
+    this.updateLists();
+  }
+
+  public changeDescription(
+    listId: string,
+    cardId: string,
+    description: string
+  ): void {
+    const lists = this.db.getData();
+
+    const list = lists.find((list) => list.id === listId);
+
+    const card = list.cards.find((card) => card.id === cardId);
+
+    card.description = description;
+
+    this.db.setData(lists);
+    this.updateLists();
+  }
+
+  public copyCard(listId: string, cardId: string): void {
+    const lists = this.db.getData();
+
+    const list = lists.find((list) => list.id === listId);
+
+    const card = list.cards.find((card) => card.id === cardId);
+
+    const copyOfCard = card.clone();
+
+    list.cards.push(copyOfCard);
+
+    this.db.setData(lists);
     this.updateLists();
   }
 
@@ -34,7 +103,7 @@ export class CardHandler extends SocketHandler {
     destinationListId: string;
   }): void {
     const lists = this.db.getData();
-    const reordered = this.reorderService.reorderCards({
+    const reordered = this.reorderProxy.reorderCards({
       lists,
       sourceIndex,
       destinationIndex,
