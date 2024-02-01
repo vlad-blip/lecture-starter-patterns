f8ef4a82d1c94294ec75e1520d7e35f1a4212e63
Vladyslav <ezio2506@gmail.com>
2024-02-01 22:23:13 +0200

    + implemented handlers for lists and card creation

--- client/src/components/column/column.tsx
+++ client/src/components/column/column.tsx
@@ -1,18 +1,24 @@
-import { colors } from '@atlaskit/theme';
+import { colors } from "@atlaskit/theme";
 import type {
   DraggableProvided,
   DraggableStateSnapshot,
-} from '@hello-pangea/dnd';
-import { Draggable } from '@hello-pangea/dnd';
+} from "@hello-pangea/dnd";
+import { Draggable } from "@hello-pangea/dnd";
 
-import type { Card } from '../../common/types';
-import { CardsList } from '../card-list/card-list';
-import { DeleteButton } from '../primitives/delete-button';
-import { Splitter } from '../primitives/styled/splitter';
-import { Title } from '../primitives/title';
-import { Footer } from './components/footer';
-import { Container } from './styled/container';
-import { Header } from './styled/header';
+import { checkIsEmpty } from "../../helpers/checkIsEmpty";
+
+import { useContext } from "react";
+import { SocketContext } from "../../context/socket";
+
+import type { Card } from "../../common/types";
+import { CardsList } from "../card-list/card-list";
+import { DeleteButton } from "../primitives/delete-button";
+import { Splitter } from "../primitives/styled/splitter";
+import { Title } from "../primitives/title";
+import { Footer } from "./components/footer";
+import { Container } from "./styled/container";
+import { Header } from "./styled/header";
+import { CardEvent, ListEvent } from "../../../../common/enums";
 
 type Props = {
   listId: string;
@@ -22,10 +28,36 @@ type Props = {
 };
 
 export const Column = ({ listId, listName, cards, index }: Props) => {
+  const socket = useContext(SocketContext);
+
+  const createCardHandler = (cardName: string) => {
+    if (checkIsEmpty(cardName)) {
+      return;
+    }
+
+    socket.emit(CardEvent.CREATE, listId, cardName);
+  };
+
+  const renameListHandler = (listName: string) => {
+    if (checkIsEmpty(listName)) {
+      return;
+    }
+
+    socket.emit(ListEvent.RENAME, listId, listName);
+  };
+
+  const deleteListHandler = () => {
+    socket.emit(ListEvent.DELETE, listId);
+  };
+
   return (
     <Draggable draggableId={listId} index={index}>
       {(provided: DraggableProvided, snapshot: DraggableStateSnapshot) => (
-        <Container className="column-container" ref={provided.innerRef} {...provided.draggableProps}>
+        <Container
+          className="column-container"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+        >
           <Header
             className="column-header"
             isDragging={snapshot.isDragging}
@@ -34,23 +66,23 @@ export const Column = ({ listId, listName, cards, index }: Props) => {
             <Title
               aria-label={listName}
               title={listName}
-              onChange={() => {}}
+              onChange={renameListHandler}
               fontSize="large"
               width={200}
               isBold
             />
             <Splitter />
-            <DeleteButton color="#FFF0" onClick={() => {}} />
+            <DeleteButton color="#FFF0" onClick={deleteListHandler} />
           </Header>
           <CardsList
             listId={listId}
             listType="CARD"
             style={{
-              backgroundColor: snapshot.isDragging ? colors.G50 : '',
+              backgroundColor: snapshot.isDragging ? colors.G50 : "",
             }}
             cards={cards}
           />
-          <Footer onCreateCard={() => {}} />
+          <Footer onCreateCard={createCardHandler} />
         </Container>
       )}
     </Draggable>
