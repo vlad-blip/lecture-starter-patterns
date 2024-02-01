aa16b965a9a2b5cb0585807f315c7275cdcf9215
Vladyslav <ezio2506@gmail.com>
2024-02-01 22:18:45 +0200

    + added listId import

--- client/src/components/card-list/components/cards.tsx
+++ client/src/components/card-list/components/cards.tsx
@@ -1,27 +1,29 @@
 import type {
   DraggableProvided,
   DraggableStateSnapshot,
-} from '@hello-pangea/dnd';
-import { Draggable } from '@hello-pangea/dnd';
-import React from 'react';
+} from "@hello-pangea/dnd";
+import { Draggable } from "@hello-pangea/dnd";
+import React from "react";
 
-import { Card } from '../../../common/types';
-import { CardItem } from '../../card-item/card-item';
+import { Card } from "../../../common/types";
+import { CardItem } from "../../card-item/card-item";
 
 type Props = {
+  listId: string;
   cards: Card[];
 };
 
-const Cards = ({ cards }: Props) => (
+const Cards = ({ listId, cards }: Props) => (
   <React.Fragment>
     {cards.map((card: Card, index: number) => (
       <Draggable key={card.id} draggableId={card.id} index={index}>
         {(
           dragProvided: DraggableProvided,
-          dragSnapshot: DraggableStateSnapshot,
+          dragSnapshot: DraggableStateSnapshot
         ) => (
           <CardItem
             key={card.id}
+            listId={listId}
             card={card}
             isDragging={dragSnapshot.isDragging}
             provided={dragProvided}
